a2fa362a52ac94ec6c7fac009988e1114e84f1d7
Betzabeth Narvaez <betzynarv@gmail.com>
2021-10-10 21:32:56 +0900

    added result texts

--- app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
+++ app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.rock_paper_scissors;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import android.os.Bundle;
 
+import com.example.rock_paper_scissors.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.paperBtn.setOnClickListener(v -> game("보"));
+        binding.rockBtn.setOnClickListener(v -> game("바위"));
+        binding.scissorsBtn.setOnClickListener(v -> game("가위"));
+
+    }
+
+    private void game(String yourChoice) {
+        int ai = random.nextInt(3);
+        String aiChoice;
+
+        switch (ai) {
+            case 0:
+                aiChoice = "가위";
+                break;
+            case 1:
+                aiChoice = "바위";
+                break;
+            default:
+                aiChoice = "보";
+        }
+
+        binding.textAI.setText("AI가 " + aiChoice + "를 냈습니다.");
+        binding.textYou.setText("당신은 " + yourChoice + "를 냈습니다.");
+
+        String result;
+
+        if (aiChoice.equals(yourChoice)) {
+            result = "비겼습니다! :/";
+        } else if ((aiChoice.equals("가위") && yourChoice.equals("바위")) || (aiChoice.equals("바위") && yourChoice.equals("보")) || (aiChoice.equals("보") && aiChoice.equals("가위"))) {
+            result = "축하합니다! :) 당신은 이겼습니다";
+        } else {
+            result = "미안합니다 :( 당신은 졌습니다";
+        }
+        binding.textResult.setText(result);
     }
 }
